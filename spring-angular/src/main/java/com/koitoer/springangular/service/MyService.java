986f40cbdf18ced97ee6a81fa712767291cda49d
koitoer <koitoer@gmail.com>
2014-04-04 23:30:42 -0700

    Adding ContextLoaderListener and service layer

---
+++ spring-angular/src/main/java/com/koitoer/springangular/service/MyService.java
@@ -0,0 +1,12 @@
+package com.koitoer.springangular.service;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MyService {
+
+	public void serviceLayer(){
+		System.out.println("Service layer is working");
+	}
+}
--- spring-angular/src/main/java/com/koitoer/springangular/web/HomeController.java
+++ spring-angular/src/main/java/com/koitoer/springangular/web/HomeController.java
@@ -1,14 +1,22 @@
 package com.koitoer.springangular.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.koitoer.springangular.service.MyService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	@Autowired
+	private MyService service;
 
 		@RequestMapping(method = RequestMethod.GET)
 		public String home(){
-			return "index"
+			service.serviceLayer();
+			return "index";
 		}
 }
